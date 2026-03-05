e2387924eec8ee93cc25d85b8a066a0ea7d87e77
Jonathan Platkiewicz <jonathan.platkiewicz@stronghold-ai.com>
2026-03-05 13:25:37 +0000

    Mock training

---
+++ src/train_sanity_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OMP_NUM_THREADS=$(nproc) \
+MKL_NUM_THREADS=$(nproc) \
+yolo task=detect mode=train \
+model=yolo11n.pt \
+data=/home/jonathan.platkiewicz/night-vision-camera-drone-detection/data.yaml \
+epochs=5 \
+imgsz=640 \
+device=0 \
+batch=16 \
+workers=$(nproc) \
+project=experiments \
+name=yolo11n-drone
